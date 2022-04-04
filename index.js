e875389bd364d89e3d0bf462b62ba894ae4647b9
Robuchon Hugues <hugues.robuchon@gmail.com>
2022-04-04 21:30:39 +0200

    petit changement réécritures création de fichier

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//module express
-let express = require('express');
-let app = express();
-
-let publicRouter = require('./routes/publicRoutes.js')
-
-//extension d'express
-let bodyParser = require('body-parser')
-
 //module dotenv
 require('dotenv').config();
+
+//module express
+const express = require('express');
+const app = express();
+
+const publicRouter = require('./routes/publicRoutes.js')
+const authRouter = require('./routes/authRoutes.js')
+
+//extension d'express
+const bodyParser = require('body-parser')
 
 //module mongoose
 require('./plugin/mongoose')
@@ -18,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //ajout des routes dans l'app
+app.use('/auth', authRouter)
 app.use('/', publicRouter)
 
 module.exports = app;
