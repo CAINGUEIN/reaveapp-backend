9b9752dbe8cad369a22bfd910542517fa8453786
Robuchon Hugues <hugues.robuchon@gmail.com>
2022-04-06 20:34:56 +0200

    passage de validation pour le model user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,57 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    email:String, 
-    password:String, //String Hasher
-    userName:String, 
-    birthDay:String,
-})
+    userName: {
+        type: String,
+        require: true,
+        minLength: 3,
+        maxLength: 30,
+        unique: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        lowercase: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    }, //String Hasher
+    birthDay:{
+        type: String,
+        require: true,
+    },
+    role: {
+        type: [String]
+    },
+    esportRole: {
+        type: [String]
+    },
+    profile: {
+        picture: {
+            type: String,
+            default: "",
+        },
+        bio: {
+            type: String,
+            maxLength: 1024,
+        },
+        followers: {
+            type: [String]
+        },
+        following: {
+            type: [String]
+        },
+        likes: {
+            type: [String]
+        },
+    },
+},
+{
+    timestamps: true,
+}
+)
 
 const UserModel = mongoose.model('user', userSchema)
 
