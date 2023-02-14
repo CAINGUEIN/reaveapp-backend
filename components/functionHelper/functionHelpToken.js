fca091d1cede24853d28811be40d7bb3e4bcd0dc
Robuchon Hugues <robuchon.hugues@orange.fr>
2023-02-14 15:01:21 +0100

    un log et un toString

--- components/functionHelper/functionHelpToken.js
+++ components/functionHelper/functionHelpToken.js
@@ -4,10 +4,11 @@ function extractToken(req, res) {
       return res.status(401).send({ success: false, message: "auth pas ok" });
     }
     const token = authHeader.split(" ")[1];
+    console.log(token);
     if (!token) {
       return res.status(401).send({ success: false, message: "token pas ok" });
     }
-    return token;
+    return token.toString();
   }
   
   module.exports = {
