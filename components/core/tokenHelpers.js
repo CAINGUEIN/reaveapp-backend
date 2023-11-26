dcf8a22dfe96f240b24ce5c96fbcd9add1501d0a
Boatti <lauris.boatti31@gmail.com>
2023-11-26 15:49:34 +0100

    test "jwt must be a string" bug

--- components/core/tokenHelpers.js
+++ components/core/tokenHelpers.js
@@ -4,7 +4,8 @@ const tools = require("../functionHelper/functionHelpToken");
 
 const TokenHelpers = {
   verifyTokenId(req, res, next) {
-    console.log("JWT verify", jwt.verify(tools.extractToken(req, res), JWT_KEY));
+    console.log(JWT_KEY);
+    //console.log("JWT verify", jwt.verify(tools.extractToken(req, res), JWT_KEY));
     let decodedToken = jwt.verify(tools.extractToken(req, res), JWT_KEY);
     if (decodedToken._id) {
       console.log("dans le OK token",decodedToken);
